cc21db6672a8084fc0882dca9abec3d3dc13822d
man <man.movin@gmail.com>
2018-04-22 22:08:13 -0300

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
-import { App, MenuController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-
 export class HomePage {
 
-  constructor(app: App, menu: MenuController) {
-    menu.enable(true);
+  constructor(public navCtrl: NavController) {
+
   }
-  openPage(page:any){
 
-  };
-  
 }
-
