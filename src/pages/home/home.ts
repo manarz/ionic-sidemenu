196c6513238f466addf6ff6e8aa6367d3f29b1d2
man <man.movin@gmail.com>
2018-04-22 21:50:44 -0300

    Version inicial sidemenu

---
+++ src/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { App, MenuController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage {
+
+  constructor(app: App, menu: MenuController) {
+    menu.enable(true);
+  }
+  openPage(page:any){
+
+  };
+  
+}
+
